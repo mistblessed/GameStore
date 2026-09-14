198489d4b1a85885f8e2be51db25c037b9264ece
mistblessed <misthyperstar@gmail.com>
2026-09-14 18:13:14 +0300

    add new blocks hero and contact to the plugin

---
+++ plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6c85656d74dfb85fc831');
---
+++ plugins/core-gamesore/core-gamestore.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Core GameStore
+ * Description: Core Code for GameStore
+ * Version: 1.0
+ * Author: mistblessed.code
+ * Author URI: https://mistblessed.code
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ */
+
+define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
